39b29320705d29b80cb6094e701cbe1203e9572c
MokhaLeee <nkulyc@163.com>
2025-01-04 08:50:46 +0000

    disassemble more

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./gbadisasm arm9.bin -l 0x02000000 -c fe11us-arm9.txt -s
+./gbadisasm arm9.bin -l 0x02000000 -c fe11us-arm9.txt -s > out-arm9.txt
 
 # commit out "MOKHA_ARM9" in gbadisasm.h
 # ./gbadisasm itcm-base.bin -l 0x01FF8000 -c fe11us-itcm.txt -s > out-itcm.txt
